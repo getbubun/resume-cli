4384d7e27cae55cc9b950cca8b03b9677a46b80d
getbubun <getbubun@gmail.com>
2019-03-18 23:09:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+"use strict";
+
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+var response = chalk.bold.green;
+
+var resume = require("./resume.json");
+
+var resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: chalk.yellow("What do you want to know about me ?"),
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+
+  console.log(" ");
+  console.log(chalk.cyan("=====================================================")); 
+  console.log(" "); 
+  console.log(chalk.white.bgGreen.bold("Hi, My name is MANAB DAS and Welcome to my CLI Resume ")); 
+  console.log(" ");
+  console.log(chalk.cyan("=====================================================")); 
+  console.log(" ");
+  
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    var option = answer.resumeOptions;
+    console.log(response("--------------------------------------"));
+    resume[`${option}`].forEach(info => {
+      console.log(response("  => " + info));
+    });
+    console.log(response("--------------------------------------"));
+    // console.log(resume[`${option}`]);
+    
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
